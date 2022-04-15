40174680bc3e7289ea34588429508c09befdf0bd
Gloria Ubah <ubahgloria16@gmail.com>
2022-04-15 06:58:12 -0700

    tasks done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prime factors of 6128
+ *
+ * Returns: int
+ */
+
+int main(void)
+{
+	unsigned long n, i, max;
+
+	n = 612852475143;
+	max = i;
+
+	for (i = 3; i <= n; i += 2)
+	{
+		while (n % i == 0)
+		{
+			max = n;
+			n = n / i;
+		}
+	}
+	printf("%lu\n", max);
+	return (0);
+}
